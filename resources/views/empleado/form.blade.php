ad1ea2288dd8acc3785ad9f716b95777bb1526a1
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-07 19:48:02 -0300

    Recuperacion de datos

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -20,28 +20,28 @@
 <div class="form-group">
 
 <label for="Nombre"> Nombre </label>
-<input type="text" class="form-control" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}" id="Nombre">
+<input type="text" class="form-control" name="Nombre" value="{{ isset($empleado->Nombre)?$empleado->Nombre:old('Nombre') }}" id="Nombre">
 
 </div>
 
 <div class="form-group">
 
 <label for="ApellidoPaterno"> Apellido paterno </label>
-<input type="text" class="form-control" name="ApellidoPaterno" value="{{  isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:'' }}" id="ApellidoPaterno">
+<input type="text" class="form-control" name="ApellidoPaterno" value="{{  isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:old('ApellidoPaterno') }}" id="ApellidoPaterno">
 
 </div>
 
 <div class="form-group">
 
 <label for="ApellidoMaterno"> Apellido materno </label>
-<input type="text" class="form-control" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:'' }}" name="ApellidoMaterno">
+<input type="text" class="form-control" name="ApellidoMaterno" value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:old('ApellidoMaterno') }}" name="ApellidoMaterno">
 
 </div>
 
 <div class="form-group">
 
 <label for="Correo"> Correo </label>
-<input type="text" class="form-control" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:'' }}" id="Correo">
+<input type="text" class="form-control" name="Correo" value="{{ isset($empleado->Correo)?$empleado->Correo:old('Correo') }}" id="Correo">
 
 </div>
 
