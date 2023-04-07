4e6fbe1375f4a6f514edb19f477ef6336734c57a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-07 19:37:37 -0300

    Validacion de datos

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -33,6 +33,22 @@ class EmpleadoController extends Controller
      */
     public function store(Request $request)
     {
+        // Validaciones
+        $campos=[
+            'Nombre'=>'required|string|max:100',
+            'ApellidoPaterno'=>'required|string|max:100',
+            'ApellidoMaterno'=>'required|string|max:100',
+            'Correo'=>'required|email',
+            'Foto'=>'required|max:10000|mimes:jpeg,png,jpg',
+        ];
+        $mensaje=[
+            'required'=>'El :attribute es requerido',
+            'Foto.required'=>'La foto es requerida'
+        ];
+
+        $this->validate($request,$campos,$mensaje);
+
+        
         // Recolecta toda la informacion del empleado
         //$datosEmpleado = request()->all();
         $datosEmpleado = request()->except('_token');
