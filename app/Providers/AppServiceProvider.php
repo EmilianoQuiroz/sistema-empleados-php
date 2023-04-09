7312c1008d15759b0f4e4e0be41d7469f698b048
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-04-09 13:09:26 -0300

    Configuracion del env para produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,15 +4,21 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     * 
+     * @return void
      */
     public function register(): void
     {
         //
+        if($this->app->enviroment('production')){
+            URL::forceScheme('https');
+        }
     }
 
     /**
